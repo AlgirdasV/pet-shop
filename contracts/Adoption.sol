e0bbe6244c210d00622534d8fefb3623e0314be8
a.vycinas <algirdas.vycinas@adform.com>
2018-04-20 00:57:33 +0300

    fix tabs

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,17 +1,17 @@
 pragma solidity ^0.4.17;
 
 contract Adoption {
-  address[16] public adopters;
+    address[16] public adopters;
 
-  function adopt(uint petId) public returns (uint) {
-    require(petId >= 0 && petId <= 15);
+    function adopt(uint petId) public returns (uint) {
+        require(petId >= 0 && petId <= 15);
 
-    adopters[petId] = msg.sender;
+        adopters[petId] = msg.sender;
 
-    return petId;
-  }
+        return petId;
+    }
   
-  function getAdopters() public view returns (address[16]) {
-    return adopters;
-  }
+    function getAdopters() public view returns (address[16]) {
+        return adopters;
+    }
 }
